71c818fd817c4804d81fe55e1e26541540ad89da
Carlos <carlos@2kpixelstudios.net>
2020-05-21 13:41:58 -0400

    Changed to .NET Framework 4.7.2 to support DynamicLua

--- CSharpCallsToLua/Program.cs
+++ CSharpCallsToLua/Program.cs
@@ -8,7 +8,6 @@ namespace CSharpCallsToLua {
 			Console.WriteLine("Welcome to a demo program that calls Lua code from C#!");
 
 			dLua = new DynamicLua.DynamicLua();
-			dLua.print = (Action<string>) Console.WriteLine;
 			dLua(@"print(""Hey from Lua!"")");
 		}
 	}
