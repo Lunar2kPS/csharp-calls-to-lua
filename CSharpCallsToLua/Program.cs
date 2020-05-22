471168fca159c53161cc575950ca27eb51d0bbd9
Carlos <carlos@2kpixelstudios.net>
2020-05-22 17:24:48 -0400

    Added actual C# code using NLua

--- CSharpCallsToLua/Program.cs
+++ CSharpCallsToLua/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using NLua;
 
 namespace CSharpCallsToLua {
 	public class Program {
-		private static dynamic dLua;
+		private static Lua luaState;
 
 		public static void Main(string[] args) {
 			Console.WriteLine("Welcome to a demo program that calls Lua code from C#!");
+
+			luaState = new Lua();
+			luaState.DoString(@"print(""Hey from Lua!!!"")");
 		}
 	}
 }
