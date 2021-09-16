3ea8b62e6d037ea619bdb5a53e042a4103f6f5c9
Carlos DaLomba <carlos@2kpixelstudios.net>
2021-09-15 23:59:41 -0400

    Reformatted C# + YAML scripts

--- CSharpCallsToLua/Program.cs
+++ CSharpCallsToLua/Program.cs
@@ -2,14 +2,14 @@
 using NLua;
 
 namespace CSharpCallsToLua {
-	public class Program {
-		private static Lua luaState;
+    public class Program {
+        private static Lua luaState;
 
-		public static void Main(string[] args) {
-			Console.WriteLine("Welcome to a demo program that calls Lua code from C#!");
+        public static void Main(string[] args) {
+            Console.WriteLine("Welcome to a demo program that calls Lua code from C#!");
 
-			luaState = new Lua();
-			luaState.DoString(@"print(""Hey from Lua!!!"")");
-		}
-	}
+            luaState = new Lua();
+            luaState.DoString(@"print(""Hey from Lua!!!"")");
+        }
+    }
 }
